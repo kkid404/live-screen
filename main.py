5560b052876d4886fb985487e4068b6d8c3c4fed
kkid404 <vizzy1488@gmail.com>
2022-05-24 22:07:13 +0500

    v 0.0.1

---
+++ main.py
@@ -0,0 +1,97 @@
+from PIL import Image, ImageDraw, ImageFont
+import ctypes
+import datetime
+from pycoingecko import CoinGeckoAPI
+import re
+import requests
+import os
+import time
+
+
+while True:
+    #Получаем погоду
+    appid = "814586ed88d8a823b94abc0979b955fd"
+    city_id= 1508291
+
+    try:
+        res = requests.get("http://api.openweathermap.org/data/2.5/weather",
+                    params={'id': city_id, 'units': 'metric', 'lang': 'ru', 'APPID': appid})
+        data = res.json()
+        conditions =  data['weather'][0]['description'].capitalize() 
+        temp =  int(data['main']['temp'])
+
+    except Exception as e:
+        print("Exception (weather):", e)
+        pass
+
+    #получем текущий курс интересующих монет на coingecko
+    cg = CoinGeckoAPI()
+
+    btc = str(cg.get_price(ids='bitcoin', vs_currencies='usd'))
+    eth = str(cg.get_price(ids='ethereum', vs_currencies='usd'))
+
+    btcprice = [float(s) for s in re.findall(r'-?\d+\.?\d*', btc)]
+    ethprice = [float(s) for s in re.findall(r'-?\d+\.?\d*', eth)]
+
+    btcprice = int(btcprice[0])
+    ethprice = int(ethprice[0])
+
+    # Получаем дни недели и перевод их в нужный вид
+    numday = datetime.datetime.today().isoweekday()
+    today = datetime.datetime.now()
+    day = today.day
+    month = today.month
+    hour = today.hour
+    minute = today.minute
+
+    def numtoweekday(int):
+        if int == 1:
+            return "Понедельник"
+        elif int == 2:
+            return "Вторник"
+        elif int == 3:
+            return "Среда"
+        elif int == 4:
+            return "Четверг"
+        elif int == 5:
+            return "Пятница"
+        elif int == 6:
+            return "Суббота"
+        elif int == 7:
+            return "Воскресенье"
+
+
+    #Получаем размер рабочего стола
+    user32 = ctypes.windll.user32
+    screensize = user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)
+    centerw = screensize[0]
+    centerh = screensize[1]
+
+    # Создаем фон изображения
+    holst = Image.new("RGB", screensize, '#231F20')
+
+
+    # Генерируем текст
+    fontweek = ImageFont.truetype("Gilroy-Semibold.ttf", size=60)
+    fontprice = ImageFont.truetype("Gilroy-Regular.ttf", size=40)
+    fontweather = ImageFont.truetype("Gilroy-Regular.ttf", size=25)
+
+    idraw = ImageDraw.Draw(holst)
+    idraw.text((centerw / 3, centerh / 2.8), numtoweekday(numday) + " " + str(day) + "."  + str(month), font=fontweek, fill="#A771FE")
+    idraw.text((centerw / 3, centerh / 2.25), "Погода в Челябинске:", font=fontweather, fill="#A771FE")
+    idraw.text((centerw / 3, centerh / 2.03), conditions + " " + str(temp), font=fontweather, fill="#A771FE")
+    idraw.text((centerw / 1.63, centerh / 2.2), str(hour) + ":" + str(minute), font=fontweek, fill="#A771FE")
+    idraw.rectangle((centerw / 3, centerh / 1.87, centerw / 1.3, centerh / 1.86), fill='#A771FE')
+    idraw.text((centerw / 3, centerh / 1.8), "BTC/USD: "+ str(btcprice), font=fontprice, fill="#e7f20c")
+    idraw.text((centerw / 3, centerh / 1.6), "ETH/USD: "+ str(ethprice), font=fontprice, fill="#e7f20c")
+    holst.save("text_pyton.jpg")
+
+
+    # Устанавливаем картинку на рабочий стол
+    folder = r"E:\python\live-screen"
+    file_name = r"text_pyton.jpg"
+    full_path = os.path.join(folder, file_name)
+    wallpaper = bytes(full_path, 'utf-8')
+    ctypes.windll.user32.SystemParametersInfoA(20, 0, wallpaper, 3)
+
+    time.sleep(60)
